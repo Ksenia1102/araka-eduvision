f84c1241d703219c8621982f55f5cfc5392315bc
Ksenia1102 <shiro.keyyu13@bk.ru>
2024-11-03 15:26:32 +0500

    first commit

---
+++ src/layout/AppSidebar.vue
@@ -0,0 +1,15 @@
+<script setup>
+import AppMenu from './AppMenu.vue';
+</script>
+
+<template>
+    <!-- тут два меню-->
+    <div class="layout-sidebar">
+        <app-menu></app-menu>
+    </div>
+    <div class="layout-sidebar">
+        <app-menu></app-menu>
+    </div>
+</template>
+
+<style lang="scss" scoped></style>
